5ce40a7b94cf245ec1c076398d9b622545064cc2
ahtashamabbasse <ch.ahtasham123@gmail.com>
2018-12-25 23:27:59 +0500

    Socket.io integration

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const path = require("path");
 const express = require('express');
-
+const socketIO = require("socket.io");
+const http = require('http')
 var port = process.env.port || 3000;
 var host = process.env.host || "127.0.0.1";
 
 
 var app = express();
 
-publicpath = path.join(__dirname, '../public');
+var server = http.createServer(app)
+var io = socketIO(server)
+
+publicpath = path.join(__dirname, 'public');
+console.log(publicpath)
 app.use(express.static(publicpath));
 
 
-app.listen(port,host, () => {
+io.on('connection', (socket) => {
+    console.log('New user connected');
+});
+
+
+server.listen(port,host, () => {
     console.log(`App is running on http://${host}:${port}`)
 })
 
